a838b1c2fd49acaef5153a42c2a5ee57968c1bf7
lucmally <lucmally@yahoo.com>
2024-01-27 08:26:20 +0000

    sonarqube error fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,36 +2,31 @@
 <html>
     <head>
         <title>Laravel</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <style>
             html, body {
                 height: 100%;
             }
-
             body {
                 margin: 0;
                 padding: 0;
                 width: 100%;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato';
+                font-family: 'Lato', sans-serif; /* Added generic font family */
             }
-
             .container {
                 text-align: center;
                 display: table-cell;
                 vertical-align: middle;
             }
-
             .content {
                 text-align: center;
                 display: inline-block;
             }
-
             .title {
                 font-size: 96px;
+                font-family: 'Lato', sans-serif; /* Added font family for the title */
             }
         </style>
     </head>
